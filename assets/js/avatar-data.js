8aa855bcc7e342b45aa5535e7b68d911d0359826
JDmitrijs7 <jdmitrijs7@gmail.com>
2026-02-11 19:37:03 +0000

    Create avatar-data.js
    Signed-off-by: JDmitrijs7 <jdmitrijs7@gmail.com>

---
+++ assets/js/avatar-data.js
@@ -0,0 +1,21 @@
+// Список аватаров JD7 (можно расширять)
+const JD7_AVATARS = [
+    {
+        id: 1,
+        name: "Ritual Alpha",
+        img: "/assets/img/avatars/avatar1.png",
+        greeting: "Добро пожаловать в ритуальный поток JD7."
+    },
+    {
+        id: 2,
+        name: "Spectral Node",
+        img: "/assets/img/avatars/avatar2.png",
+        greeting: "Синхронизация завершена. Канал открыт."
+    },
+    {
+        id: 3,
+        name: "Neon Oracle",
+        img: "/assets/img/avatars/avatar3.png",
+        greeting: "Трансформация активирована. Готов к действию."
+    }
+];
