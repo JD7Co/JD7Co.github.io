ea34a4ac4e4fc19a9970a6fd89e0af82e621cc58
JDmitrijs7 <jdmitrijs7@gmail.com>
2026-02-11 19:03:59 +0000

    Create avatar-engine.js
    Signed-off-by: JDmitrijs7 <jdmitrijs7@gmail.com>

---
+++ assets/js/avatar-engine.js
@@ -0,0 +1,9 @@
+const JD7AvatarEngine = {
+    init() {
+        console.log('JD7 Avatar Engine ready');
+    }
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    JD7AvatarEngine.init();
+});
