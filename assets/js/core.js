adc05039207a9da06f66b53870401842144c2966
JDmitrijs7 <jdmitrijs7@gmail.com>
2026-02-11 19:15:20 +0000

    Update core.js
    Signed-off-by: JDmitrijs7 <jdmitrijs7@gmail.com>

--- assets/js/core.js
+++ assets/js/core.js
@@ -1,3 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('JD7 Portal v3.0 loaded');
 });
+function loadTemplate() {
+    fetch('/modules/template.html')
+        .then(response => response.text())
+        .then(html => {
+            document.getElementById('header').innerHTML = html.split('</header>')[0] + '</header>';
+            document.getElementById('footer').innerHTML = html.split('</header>')[1];
+        });
+}
