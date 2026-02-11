8cb40c2be514689c442d87dcb38ae1831038a5a4
JDmitrijs7 <jdmitrijs7@gmail.com>
2026-02-11 19:16:01 +0000

    Update ritual.js
    Signed-off-by: JDmitrijs7 <jdmitrijs7@gmail.com>

--- assets/js/ritual.js
+++ assets/js/ritual.js
@@ -2,3 +2,11 @@ function ritualPulse(element) {
     if (!element) return;
     element.classList.add('ritual-glow');
 }
+function loadTemplate() {
+    fetch('/modules/template.html')
+        .then(response => response.text())
+        .then(html => {
+            document.getElementById('header').innerHTML = html.split('</header>')[0] + '</header>';
+            document.getElementById('footer').innerHTML = html.split('</header>')[1];
+        });
+}
