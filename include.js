2d0c6753912df9c40e680a1e17b9eb33b75dcadd
JDmitrijs7 <jdmitrijs7@gmail.com>
2026-03-19 06:24:26 +0000

    Update include.js

--- include.js
+++ include.js
@@ -5,3 +5,17 @@ document.addEventListener("DOMContentLoaded", () => {
     el.innerHTML = await response.text();
   });
 });
+
+async function getGasFees() {
+  const response = await fetch(`${GAS_API}/fee-history`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      blockCount: 5,
+      newestBlock: "latest",
+      rewardPercentiles: [5, 50, 95]
+    })
+  });
+
+  return await response.json();
+}
